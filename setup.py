c29d0bd17de64c843e208a13b09e2d528f8d662f
CUDA <pinto@mit.edu>
2009-01-22 01:50:37 -0500

    sudo python setup.py develop && python -c "from cuda import cuda,cu,cublas,cufft,utils" now works

--- setup.py
+++ setup.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
-# coding:utf-8: © Arno Pähler, 2007-08
+# -*- coding: utf-8 -*- 
 
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools(version='0.6c9')
+
+from setuptools import setup, find_packages
+
+print find_packages('cuda')
 
 setup(
-    name='python-cuda',
-    version='2.0_42',
-    author='Arno Pähler',
-    author_email='paehler@graviscom.de',
-    url='ftp://ftp.graviscom.de/pub/python-cuda/',
-    description='ctypes Python bindings for NVidia CUDA',
-    long_description = """\
-The ctypes Python bindings are created from the header files,
-distributed with NVidia's CUDA SDK. They implement both the
-driver and the runtime API as well as part of the BLAS library.
-Some SDK examples have been ported and other test examples been
-created and tested with GeForce 8500GT and GeForce 8600GTS cards
-under CUDA versions 1.0 and 1.1.
-The latest version have support for CUBLAS (except complex cases)
-and CUFFT. Support for the CUDA Application Interface is still
-incomplete (things like something<<<dim,dim>>> not supported).
-Python bindings to FFTW 2.x are also supplied to use in testing
-CUFFT examples.
-""",
-    download_url='ftp://ftp.graviscom.de/pub/python-cuda/',
-    license='LGPL',
-    package_dir = {'cuda':'src/cuda'},
-    packages=['cuda','cuda.utils','cuda.examples'],
-    package_data = {'cuda':['MANIFEST.in','README'],
-                    'cuda.examples':
-                    ['*.so','*.c','*.h','*.cu*','*.ptx','compile*']}
+    name = 'python-cuda',
+    version = '2.1-jrnp001',
+    
+    packages = find_packages('cuda'),
+    package_dir = {'': 'cuda'}
+
+#     author='Arno Pähler',
+#     author_email='paehler@graviscom.de',
+#     url='ftp://ftp.graviscom.de/pub/python-cuda/',
+#     description='ctypes Python bindings for NVidia CUDA',
+#     long_description = """\
+# The ctypes Python bindings are created from the header files,
+# distributed with NVidia's CUDA SDK. They implement both the
+# driver and the runtime API as well as part of the BLAS library.
+# Some SDK examples have been ported and other test examples been
+# created and tested with GeForce 8500GT and GeForce 8600GTS cards
+# under CUDA versions 1.0 and 1.1.
+# The latest version have support for CUBLAS (except complex cases)
+# and CUFFT. Support for the CUDA Application Interface is still
+# incomplete (things like something<<<dim,dim>>> not supported).
+# Python bindings to FFTW 2.x are also supplied to use in testing
+# CUFFT examples.
+# """,
+#     download_url='ftp://ftp.graviscom.de/pub/python-cuda/',
+#     license='LGPL',
+#     package_dir = {'cuda':'src/cuda'},
+#     packages=['cuda','cuda.utils','cuda.examples'],
+#     package_data = {'cuda':['MANIFEST.in','README'],
+#                     'cuda.examples':
+#                     ['*.so','*.c','*.h','*.cu*','*.ptx','compile*']}
 )
--- src/cuda/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [
-"cu_api","cu_defs",
-"cuda_api","cuda_defs",
-"cublas_api","cublas_defs",
-"cpuFuncs","libc","mklMath"]
-
-
