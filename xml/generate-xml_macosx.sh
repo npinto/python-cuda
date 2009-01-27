b119e4dc161573d615f4a0581ad854ca293cbcc2
CUDA <justin.t.riley@gmail.com>
2009-01-27 16:19:19 -0500

    added -I option for generate-xml_macosx.sh

--- xml/generate-xml_macosx.sh
+++ xml/generate-xml_macosx.sh
@@ -2,10 +2,10 @@
 
 # NP: do we need a python script for that ?
 
-python createbindings.py -H /usr/local/cuda/include/cuda.h -l /usr/local/cuda/lib/libcuda.dylib -x cu.xml -p ../cuda/cu/cudadrv.py
-python createbindings.py -H /usr/local/cuda/include/cuda_runtime.h -l /usr/local/cuda/lib/libcudart.dylib -x cudart.xml -p ../cuda/cuda/cudart.py
-python createbindings.py -H /usr/local/cuda/include/cublas.h -l /usr/local/cuda/lib/libcublas.dylib -x cublas.xml -p ../cuda/cublas/cublas.py
-python createbindings.py -H /usr/local/cuda/include/cufft.h -l /usr/local/cuda/lib/libcufft.dylib -x cufft.xml -p ../cuda/cufft/cufft.py
+python createbindings.py -H /usr/local/cuda/include/cuda.h -l /usr/local/cuda/lib/libcuda.dylib -I /usr/local/cuda/include -x cu.xml -p ../cuda/cu/cudadrv.py
+python createbindings.py -H /usr/local/cuda/include/cuda_runtime.h -l /usr/local/cuda/lib/libcudart.dylib -I /usr/local/cuda/include -x cudart.xml -p ../cuda/cuda/cudart.py
+python createbindings.py -H /usr/local/cuda/include/cublas.h -l /usr/local/cuda/lib/libcublas.dylib -I /usr/local/cuda/include -x cublas.xml -p ../cuda/cublas/cublas.py
+python createbindings.py -H /usr/local/cuda/include/cufft.h -l /usr/local/cuda/lib/libcufft.dylib -I /usr/local/cuda/include -x cufft.xml -p ../cuda/cufft/cufft.py
 
 #find . -iname \*.py -exec python {} \;
 
