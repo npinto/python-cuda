64ffd58e9302af72da60bc1309ebc515f17a6bf2
thor <pinto@mit.edu>
2009-01-27 20:53:01 -0500

    corrected createbindings on linux

--- xml/generate-xml_linux.sh
+++ xml/generate-xml_linux.sh
@@ -2,14 +2,14 @@
 
 # NP: do we need a python script for that ?
 
-python createbindings.py -H /usr/local/cuda/include/cuda.h -l /usr/lib/libcuda.so -x cu.xml -p ../cuda/cu/cudadrv.py
-python createbindings.py -H /usr/local/cuda/include/cuda_runtime.h -l /usr/local/cuda/lib/libcudart.so -x cudart.xml -p ../cuda/cuda/cudart.py
-python createbindings.py -H /usr/local/cuda/include/cublas.h -l /usr/local/cuda/lib/libcublas.so -x cublas.xml -p ../cuda/cublas/cublas.py
-python createbindings.py -H /usr/local/cuda/include/cufft.h -l /usr/local/cuda/lib/libcufft.so -x cufft.xml -p ../cuda/cufft/cufft.py
+python createbindings.py -H /usr/local/cuda/include/cuda.h -l /usr/lib/libcuda.so -x cu.xml -p cudadrv.py && \
+python cudadrv.py && cp -vf cudadrv.py ../cuda/cu/
 
-#find . -iname \*.py -exec python {} \;
+python createbindings.py -H /usr/local/cuda/include/cuda_runtime.h -l /usr/local/cuda/lib/libcudart.so -x cudart.xml -p cudart.py && \
+python cudart.py && cp -vf cudart.py ../cuda/cuda/
 
-python ../cuda/cu/cudadrv.py
-python ../cuda/cuda/cudart.py
-python ../cuda/cublas/cublas.py
-python ../cuda/cufft/cufft.py
+python createbindings.py -H /usr/local/cuda/include/cublas.h -l /usr/local/cuda/lib/libcublas.so -x cublas.xml -p cublas.py && \
+
+python createbindings.py -H /usr/local/cuda/include/cufft.h -l /usr/local/cuda/lib/libcufft.so -x cufft.xml -p cufft.py && \
+python cufft.py && cp -vf cufft.py ../cuda/cufft/
+
