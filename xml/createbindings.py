99aef6ba3faef49731f4a1709643b89d2fb57c6e
thor <pinto@mit.edu>
2009-01-28 01:00:06 -0500

    modified bindings creation scripts to handle dim3 struct properly

--- xml/createbindings.py
+++ xml/createbindings.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
 import re
-from sys import argv,exit
-from os import waitpid
-from os.path import basename,dirname
-from subprocess import Popen, PIPE
+import os, sys
 from optparse import OptionParser
+from subprocess import Popen, PIPE
 
 def fix_cdll_imports(bindings, lib):
     cdll_regex = "_libraries['%s'] = CDLL('%s')" % (lib,lib)
     new_bindings = []
-    libname = basename(lib).split('.')[0].replace('lib','')
+    libname = os.path.basename(lib).split('.')[0].replace('lib','')
     for line in bindings:
         if line.rfind(cdll_regex) != -1:
-            new_bindings.append(line.replace(cdll_regex, "_libraries['%s'] = get_lib('%s')" % (libname,libname)))
+            new_bindings += [line.replace(cdll_regex, "_libraries['%s'] = get_lib('%s')" % (libname,libname))]
         else:
-            new_bindings.append(line.replace(lib,libname))
+            new_bindings += [line.replace(lib,libname)]
     new_bindings.insert(3,'from cuda.utils import get_lib\n')
     new_bindings.insert(4,'c_longdouble = c_double\n')
     return new_bindings
@@ -31,97 +29,79 @@ def main(args=None):
         -p python_output-file
     """  
     if args is None:
-        args = argv
+        args = sys.argv
 
-    usage = "usage: %prog -H header_file -l library_file [options]"
+    usage = "usage: %prog [options]"
     parser = OptionParser(usage)
-    parser.add_option("-H","--header-file",
-                      dest="HEADER_FILE",
-                      help="(required)full path to header file (e.g /usr/local/cuda/include/cuda.h)",
-                      default = None)
+    parser.add_option("-H","--header",
+                      action="append",
+                      dest="HEADERS",
+                      help="[REQUIRED] full path to header (e.g /path/to/foo.h)",
+                      default = [])
 
-    parser.add_option("-l","--library-file",
+    parser.add_option("-l","--library",
                       action="append",
                       dest="LIBRARIES",
-                      help="(required)full path to library file (e.g. /usr/local/cuda/lib/libcuda.so) ",
+                      help="[REQUIRED] full path to library (e.g /path/to/libfoo.so)",
                       default = [])
 
     parser.add_option("-I","--include-dir",
                       action="append",
                       dest="INCLUDE_DIRS",
-                      help="paths to find dependency headers (e.g /usr/local/cuda/include/)",
+                      help="[REQUIRED] paths to find dependency headers (e.g /usr/local/cuda/include/)",
                       default = [])
 
     parser.add_option("-x","--xml-output-file",
                       dest="XML_FILE",
                       help="file to store xml output to (e.g. /my/project/xml/cuda.xml)",
-                      default = None)
+                      default = "output.xml")
 
     parser.add_option("-p","--python-output-file",
                       dest="PYTHON_FILE",
                       help="file to store python bindings to (e.g. /my/project/cuda.py)",
-                      default = None)
+                      default = "output.py")
 
-    (options,args) = parser.parse_args(args[1:])
-    
-    if options.HEADER_FILE is None or options.LIBRARIES is None: 
-        print 'ERROR: You must supply both the header file (-H) and the library file (-l)'
-        exit()
+    options, args = parser.parse_args()
+        
+    if len(options.HEADERS) == 0 or \
+            len(options.LIBRARIES) == 0 or \
+            len(options.INCLUDE_DIRS) == 0:
+        raise ValueError, "You must supply the header files (-H), the library files (-l) and the include dirs (-I)"
 
-    if options.XML_FILE is None:
-        if options.HEADER_FILE is not None:
-            options.XML_FILE = basename(options.HEADER_FILE) + '.xml'
-        else:
-            options.XML_FILE = "output.xml"
+    headers = " ".join(options.HEADERS)
+    libraries = "-l " + " -l ".join(options.LIBRARIES)
+    include_dirs = "-I " + " -I ".join(options.INCLUDE_DIRS)
+    xml_file = options.XML_FILE
+    python_file = options.PYTHON_FILE
 
-    if options.PYTHON_FILE is None:
-        if options.LIBRARIES is not None:
-            options.PYTHON_FILE = basename(options.LIBRARIES) + '.py'
-        else:
-            options.PYTHON_FILE = "output.py"
-
-    if len(options.INCLUDE_DIRS) == 0:
-        options.INCLUDE_DIRS = dirname(options.HEADER_FILE)
-    else:
-        inc_opts = ""
-        for inc in options.INCLUDE_DIRS:
-            inc_opts += ' -I %s' % inc
-        options.INCLUDE_DIRS=inc_opts
-    
-    library_options = []
-    for lib in options.LIBRARIES:
-        library_options.append('-l %s' % lib)
-
-    config = {'HEADER_FILE':options.HEADER_FILE, 
-              'XML_FILE': options.XML_FILE, 
-              'PYTHON_FILE': options.PYTHON_FILE, 
-              'INCLUDE': options.INCLUDE_DIRS}
-
-    h2xmlcmd = 'python -m ctypeslib.h2xml %(HEADER_FILE)s' + \
-        ' -I %(INCLUDE)s -o %(XML_FILE)s'
-    h2xmlcmd = h2xmlcmd % config
+    # -- h2xml
+    h2xmlcmd_l = ["python -m ctypeslib.h2xml"]
+    h2xmlcmd_l += [headers]
+    h2xmlcmd_l += [include_dirs]
+    h2xmlcmd_l += ["-o %s" % xml_file]
+    h2xmlcmd = " ".join(h2xmlcmd_l)
     print "h2xmlcmd =", h2xmlcmd
-    p = Popen(h2xmlcmd, shell=True)
-    sts = waitpid(p.pid, 0)
+    assert os.system(h2xmlcmd) == 0
 
     # XXX: hack to remove Converter tags from the xml file
-    # before Justin's patch gets accepted in ctypeslib
-    lines = [line for line in open(config['XML_FILE']).readlines() 
+    lines = [line for line in open(xml_file).readlines() 
              if not line.startswith('  <Converter id="_')]
-    open(config['XML_FILE'], 'w+').writelines(lines)
+    open(xml_file, 'w+').writelines(lines)
     
-    xml2pycmd = ('python -m ctypeslib.xml2py %(XML_FILE)s -c -d -v ' \
-                     % config) + ' '.join(library_options)
-    print "xml2pycmd = %s" % xml2pycmd
+    # -- xml2py
+    xml2pycmd_l = ["python -m ctypeslib.xml2py"]
+    xml2pycmd_l += [xml_file]
+    xml2pycmd_l += ["-c -d -v"]
+    xml2pycmd_l += [libraries]
+    xml2pycmd = " ".join(xml2pycmd_l)
+    print "xml2pycmd =", xml2pycmd
     p = Popen(xml2pycmd, shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
     bindings = p.stdout.readlines()
 
     for lib in options.LIBRARIES:
         bindings = fix_cdll_imports(bindings, lib)
 
-    file = open(config['PYTHON_FILE'], 'w')
-    file.writelines(bindings)
-    file.close()
+    open(python_file, 'w').writelines(bindings)
 
 if __name__ == "__main__":
     main()
--- xml/generate-xml_linux.sh
+++ xml/generate-xml_linux.sh
@@ -2,14 +2,17 @@
 
 # NP: do we need a python script for that ?
 
-python createbindings.py -H /usr/local/cuda/include/cuda.h -l /usr/lib/libcuda.so -x cu.xml -p cudadrv.py && \
+INCLUDES="-I ./ -I /usr/local/cuda/include"
+
+python createbindings.py -H my_CUDA2100_vector_types.h -H cuda.h -l /usr/lib/libcuda.so $INCLUDES -x cudadrv.xml -p cudadrv.py && \
 python cudadrv.py && cp -vf cudadrv.py ../cuda/cu/
 
-python createbindings.py -H /usr/local/cuda/include/cuda_runtime.h -l /usr/local/cuda/lib/libcudart.so -x cudart.xml -p cudart.py && \
+python createbindings.py -H my_CUDA2100_vector_types.h -H cuda_runtime.h -l /usr/local/cuda/lib/libcudart.so $INCLUDES -x cudart.xml -p cudart.py && \
 python cudart.py && cp -vf cudart.py ../cuda/cuda/
 
-python createbindings.py -H /usr/local/cuda/include/cublas.h -l /usr/local/cuda/lib/libcublas.so -x cublas.xml -p cublas.py && \
+python createbindings.py -H my_CUDA2100_vector_types.h -H cublas.h -l /usr/local/cuda/lib/libcublas.so $INCLUDES -x cublas.xml -p cublas.py && \
+python cublas.py && cp -vf cublas.py ../cuda/cublas/
 
-python createbindings.py -H /usr/local/cuda/include/cufft.h -l /usr/local/cuda/lib/libcufft.so -x cufft.xml -p cufft.py && \
+python createbindings.py -H my_CUDA2100_vector_types.h -H cufft.h -l /usr/local/cuda/lib/libcufft.so $INCLUDES -x cufft.xml -p cufft.py && \
 python cufft.py && cp -vf cufft.py ../cuda/cufft/
 
