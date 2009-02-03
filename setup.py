8a5807c636a1f8c23a0be2509cb9c6ee78ce9b75
thor <pinto@mit.edu>
2009-02-02 21:17:45 -0500

    pylinted setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+
+""" setuptools setup.py for python-cuda """
 
 from ez_setup import use_setuptools
 use_setuptools(version='0.6c9')
@@ -8,9 +10,18 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'python-cuda',
+
     version = '2.1-0.0.1',
 
-    packages= ['cuda','cuda.cu','cuda.cuda', 'cuda.cublas', 'cuda.cufft', 'cuda.kernel', 'cuda.array', 'cuda.utils'],
+    packages = ['cuda',
+                'cuda.cu',
+                'cuda.cuda', 
+                'cuda.cublas', 
+                'cuda.cufft', 
+                'cuda.kernel', 
+                'cuda.array', 
+                'cuda.utils'],
+
     package_dir = {'cuda':'cuda'},
 
 #     author='',
