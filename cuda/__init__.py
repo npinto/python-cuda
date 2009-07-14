6c0fccd7a20d46e1260d08552f323335b66e0c2d
Justin Riley <justin.t.riley@gmail.com>
2009-07-14 00:56:58 -0400

    __init__.py now adds cuda to path for windows
    added check in __init__.py for windows that fetches the cuda install dir from the registry and adds it to the path

--- cuda/__init__.py
+++ cuda/__init__.py
@@ -15,3 +15,14 @@ import sugar
 import logging
 logging.basicConfig(level=logging.DEBUG)
 #logging.disable(logging.ERROR)
+
+import platform
+
+# add $CUDA_ROOT/bin to %PATH% in windows
+if platform.system() == "Windows":
+    import _winreg as wreg
+    reg = wreg.ConnectRegistry(None, wreg.HKEY_LOCAL_MACHINE)
+    key = wreg.OpenKey(reg, r"SOFTWARE\NVIDIA Corporation\Installed Products\NVIDIA CUDA")
+    cuda_bin = os.path.join(wreg.QueryValueEx(key, "InstallDir")[0],"bin")
+    import os
+    os.environ['PATH'] += os.path.join(os.path.pathsep, cuda_bin)
